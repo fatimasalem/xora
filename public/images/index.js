f1a6e0888022046359c24d465592182e9ced6a6c
FatimaSalem <FatimaSalem421999@gmail.com>
2024-11-24 12:36:50 +0400

    first commit

---
+++ public/images/index.js
@@ -0,0 +1,15 @@
+import boy from "./boy.jpg";
+import girlwithglasses from "./girlwithglasses.jpg";
+import girlwithlonghair from "./girlwithlonghair.jpg";
+import oldman from "./oldman.jpg";
+import girl from "./girl.jpg";
+import blondgirl from "./blondgirl.jpg";
+
+export {
+	boy,
+	girlwithglasses,
+	girlwithlonghair,
+	oldman,
+	blondgirl,
+	girl
+}
